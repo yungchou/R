c086b2e568a4c1d4cadcb1c08d502abecc2fa2af
yung chou <10538409+yungchou@users.noreply.github.com>
2018-10-10 22:32:10 -0400

    Update global.R

--- shiny/shiny.iot.base/global.R
+++ shiny/shiny.iot.base/global.R
@@ -17,10 +17,3 @@ latitude=c(33,40,37,34)
 sensorID=c("rp1","rp2","rp3","rp4")
 device <- cbind (sensorID, longitude, latitude)
 
-# source(credentials.R)
-# For testing
-sender <- '@txt.att.com'
-recipients <- c('yc2121@live.com', '7043403545@txt.att.net')
-password = "password"
-
-
